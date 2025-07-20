7feb1b228abd2e765a62a06a818daaeaf0377946
Korball <nahidwin701@gmail.com>
2025-07-20 09:23:35 +0000

    add an empty side panel for now

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,12 @@ export default function MainLayout({
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <AppHeader />
-      <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
+      <div className="flex flex-1 container mx-auto max-w-7xl">
+        <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
+        <aside className="hidden lg:block w-80 border-l border-border/40 p-6">
+          {/* Side Panel Content */}
+        </aside>
+      </div>
     </div>
   );
 }
